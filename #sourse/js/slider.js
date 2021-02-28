e449cb22912f0df8e3356b11e9928d2ccec2ab55
Elena <mesheryakova-elena90@yandex.ru>
2021-02-28 17:02:09 +0300

    второй слайдер и блок контакты

--- #sourse/js/slider.js
+++ #sourse/js/slider.js
@@ -4,5 +4,18 @@ $(function(){
      arrows: false,
      dots: true,
    });
+   $('.slider__offer-inner').slick({
+      centerMode: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true,
+    });
+
+    $('.slider__events-inner').slick({
+      arrows: true,
+      dots: false,
+    });
 
  });
--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,1 @@
-$(function(){$(".slider__inner").slick({arrows:!1,dots:!0})});
+$(function(){$(".slider__inner").slick({arrows:!1,dots:!0}),$(".slider__offer-inner").slick({centerMode:!0,infinite:!0,slidesToShow:3,slidesToScroll:1,arrows:!1,dots:!0}),$(".slider__events-inner").slick({arrows:!0,dots:!1})});
